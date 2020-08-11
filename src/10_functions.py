6b5503cd16bef1be3ebdf852885ca3a6a5185220
Cobrettie <cobrettiegarner3@gmail.com>
2020-08-10 21:06:02 -0400

    complete 10_functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,5 +1,12 @@
 # Write a function is_even that will return true if the passed-in number is even.
 # YOUR CODE HERE
+def is_even(n):
+    if int(n) % 2 == 0:
+        print(True)
+    else:
+        print(False)
+
+is_even(2)
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -7,9 +14,20 @@ num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 # YOUR CODE HERE
+def is_even_v2(n):
+    if int(n) % 2 == 0:
+        print('Even!')
+    else:
+        print('Odd')
 
-# Read a string from the keyboard
+is_even_v2(num)
+
+# # Read a string from the keyboard
 s = input("Enter some characters: ")
 
-# Write a function that reverses the input string s
-# YOUR CODE HERE
+# # Write a function that reverses the input string s
+# # YOUR CODE HERE
+def reverse_string(s):
+    print(''.join(reversed(s)))
+
+reverse_string(s)
